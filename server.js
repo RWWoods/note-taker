7428f78597521f4b0a1fdc56790aaa53449e9b8e
Kiggles <ridgewwoods@gmail.com>
2023-04-20 22:04:33 -0400

    worked with tutor (robert) to figure out get and post better, cleaning up some uselss code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// this reads our notes database when on the right url to load any notes that are saved for the user to see
 app.get('/api/notes/', (req, res) => {
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
         if (err) {
@@ -24,10 +24,10 @@ app.get('/api/notes/', (req, res) => {
     console.info(`${req.method} request received to get notes`);
 })
 
-
+// this reads the contents (body) of a new note being written to db.json and writes it to the page
 app.post('/api/notes', (req, res) => {
     res.status(200).json(`${req.method} request received to add a note`);
-    // console.info(`${req.method} request received to add a note`);
+
 
     const { title, text } = req.body;
 
@@ -41,13 +41,13 @@ app.post('/api/notes', (req, res) => {
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 throw err
-            } else{
+            } else {
                 let parsedNotes = JSON.parse(data);
 
                 parsedNotes.push(newNote);
 
                 fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) =>
-                writeErr ? console.error(writeErr) : console.info("successful update"))
+                    writeErr ? console.error(writeErr) : console.info("successful update"))
             }
         })
 
@@ -55,7 +55,29 @@ app.post('/api/notes', (req, res) => {
         res.error(`error adding note`)
     }
 })
+// this delete request allows the user to use the trashcan icon to remove a note from the list.
+app.delete('/api/notes/:id', (req, res) => {
+    const deleteId = req.params.id;
 
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err
+        } else {
+            let parsedNotes = JSON.parse(data);
+            const arrayId = parsedNotes.filter(note => note.id != deleteId)
+           
+
+            fs.writeFile('./db/db.json', JSON.stringify(arrayId, null, 4), (writeErr) =>
+                writeErr ? console.error(writeErr) : res.json(JSON.stringify(arrayId)))
+        }
+
+    })
+
+
+})
+
+// these get requests send responses on what html page will load based on url.
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -68,5 +90,5 @@ app.get('/notes', (req, res) =>
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
+// sets server to 'live' basically to respond to requests
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
